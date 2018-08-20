e39bc4316896e7abe27ee248b4d098a5d433e5d7
tuanpq <quoctuan931113@gmail.com>
2018-08-20 23:38:09 +0800

    add Login.csproj PQTTool.Library.dll

---
+++ source/THUOCBACWPF/PQTTool/Helpers/ConverterBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PQTTool.Helpers {
+ public class ConverterBase:IMultiValueConverter {
+	public object Convert(object[] values,Type targetType,object parameter,System.Globalization.CultureInfo culture) {
+	  return values.Clone();
+	}
+
+	public object[] ConvertBack(object value,Type[] targetTypes,object parameter,System.Globalization.CultureInfo culture) {
+	  return null;
+	}
+  }
+
+  public class Boolean2VisibilityConverter:IValueConverter {
+	public object Convert(object value,Type targetType,object parameter,CultureInfo culture) {
+	  return (bool)value ? Visibility.Visible : Visibility.Hidden;
+	}
+
+	public object ConvertBack(object value,Type targetType,object parameter,CultureInfo culture) {
+	  return (Visibility)value == Visibility.Visible;
+	}
+  }
+
+}
---
+++ source/THUOCBACWPF/PQTTool/Helpers/Helpers.cs
@@ -0,0 +1,14 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PQTTool.Helpers {
+ public class Helpers {
+	public static T DeepCopy<T>(T other) {
+	  var objectJson = JsonConvert.SerializeObject(other);
+	  return JsonConvert.DeserializeObject<T>(objectJson);
+	}
+  }
+}
