60f7d75dfbb8abd9b0cbd321c0672fb3ff47de5a
tuanpq <quoctuan931113@gmail.com>
2020-11-13 23:09:04 +0800

    update DNQTDataAccessLayer.csproj add DAL_Account.cs

--- DRUGNORTHQT/Clients/DNQTDataAccessLayer/Connection.cs
+++ DRUGNORTHQT/Clients/DNQTDataAccessLayer/Connection.cs
@@ -1,15 +1,17 @@
-﻿using System.Configuration;
+﻿using QT.Framework.ToolCommon;
 using System.Data;
 using System.Data.SqlClient;
 
 namespace DNQTDataAccessLayer {
   public class Connection {
+
 	public SqlConnection con;
 	public SqlCommand cmd;
 	public SqlDataAdapter dap;
 	public DataSet dt;
 	public DataTable dataTableBangDuLieu;
 	public static string strcon="";
+
 	public Connection() {
 	  //strcon = System.Configuration.ConfigurationManager.ConnectionStrings["CTSDBConnection"].ConnectionString;
 	  //strcon = "Data Source=qt;" + "Initial Catalog=SmsGateway159;" + "User id=sa;" + "Password=1234$;";
@@ -18,20 +20,29 @@ namespace DNQTDataAccessLayer {
 	  //public static string strKetNoiCoSoDuLieu = "Data Source=115.84.178.37;" + "Initial Catalog=Autocare_Dev;" + "User id=autocaredev;" + "Password=autocaredev123!@#;";
 	  //strcon="Data Source=115.84.178.37;"+"Initial Catalog=Autocare_Dev;"+"User id=autocaredev;"+"Password=autocaredev123!@#;";
 	  //strcon="Data Source=qt;"+"Initial Catalog=Autocare2;"+"User id=sa;"+"Password=1234$;";
-	  strcon=ConfigurationManager.ConnectionStrings["ConnectionThuocBac"].ConnectionString;
+	  //strcon=ConfigurationManager.ConnectionStrings["ConnectionThuocBac"].ConnectionString;
 	  //static string strConStr = "Data Source=115.84.178.37;" + "Initial Catalog=Autocare_Dev;" + "User id=autocaredev;" + "Password=autocaredev123!@#;";
-	  con=new SqlConnection(strcon);
+
+	  string strConnection = "";
+	  GetStringConnectionDecodeFromConfig(ref strConnection);
+	  strcon=strConnection;
+
+	  con =new SqlConnection(strcon);
 	  cmd=con.CreateCommand();
 	}
+
 	public void OpenConnection() {
 	  if(con.State!=ConnectionState.Open)
 		con.Open();
 	}
+
 	public void CloseConnection() {
 	  //if (con.State == ConnectionState.Open)
 	  //    con.Close();
 	}
-	public DataTable dataTableThucThiQuery(string strSQL,CommandType cmdT,params SqlParameter[] param) {
+
+	public DataTable dataTableThucThiQuery(string strSQL,CommandType cmdT
+	  ,params SqlParameter[] param) {
 	  cmd.Parameters.Clear();
 	  OpenConnection();
 	  cmd.Connection=con;
@@ -50,7 +61,9 @@ namespace DNQTDataAccessLayer {
 	  CloseConnection();
 	  return dataTableBangDuLieu;
 	}
-	public DataSet ExecuteQueryDataSet(string strSQL,CommandType cmdT,params SqlParameter[] param) {
+
+	public DataSet ExecuteQueryDataSet(string strSQL,CommandType cmdT
+	  ,params SqlParameter[] param) {
 	  cmd.Parameters.Clear();
 	  OpenConnection();
 	  cmd.Connection=con;
@@ -68,7 +81,9 @@ namespace DNQTDataAccessLayer {
 	  CloseConnection();
 	  return dt;
 	}
-	public bool blnThucThiNonQuery(string strSQL,CommandType cmdT,ref string err,params SqlParameter[] param) {
+
+	public bool blnThucThiNonQuery(string strSQL,CommandType cmdT,ref string err
+	  ,params SqlParameter[] param) {
 	  cmd.Parameters.Clear();
 	  bool f=false;
 	  OpenConnection();
@@ -93,7 +108,9 @@ namespace DNQTDataAccessLayer {
 	  }
 	  return f;
 	}
-	public bool boolThucThiNonQuery(string strSQL,CommandType cmdT,ref string err,ref string result,params SqlParameter[] param) {
+
+	public bool boolThucThiNonQuery(string strSQL,CommandType cmdT,ref string err
+	  ,ref string result,params SqlParameter[] param) {
 	  cmd.Parameters.Clear();
 	  bool f=false;
 	  OpenConnection();
@@ -122,7 +139,9 @@ namespace DNQTDataAccessLayer {
 	  }
 	  return f;
 	}
-	public bool boolThucThiScalarSql(string strSQL,CommandType cmdT,ref string err,ref string result,params SqlParameter[] param) {
+
+	public bool boolThucThiScalarSql(string strSQL,CommandType cmdT,ref string err
+	  ,ref string result,params SqlParameter[] param) {
 	  cmd.Parameters.Clear();
 	  bool f=false;
 	  OpenConnection();
@@ -147,7 +166,9 @@ namespace DNQTDataAccessLayer {
 	  }
 	  return f;
 	}
-	public object objectThucThiScalar(string strSQL,CommandType cmdT,ref string err,ref string result,params SqlParameter[] param) {
+
+	public object objectThucThiScalar(string strSQL,CommandType cmdT,ref string err
+	  ,ref string result,params SqlParameter[] param) {
 	  cmd.Parameters.Clear();
 	  //bool f = false;
 	  //object kq=null;
@@ -174,5 +195,31 @@ namespace DNQTDataAccessLayer {
 	  //return f;
 	  return result;
 	}
+
+	internal void GetStringConnectionDecodeFromConfig(ref string strConnection) {
+	  strConnection="";
+	  try {
+		string strValueKey = "";
+		BLLTools.GetValueFromFileConfig(ref strValueKey,"IpPortConnect");
+		strConnection=Base64Decode(strValueKey);
+	  } catch(System.Exception e) {
+		string str = e.Message;
+	  }
+	}
+
+	#region Encode decode base64
+
+	private string Base64Encode(string plainText) {
+	  var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+	  return System.Convert.ToBase64String(plainTextBytes);
+	}
+
+	private string Base64Decode(string base64EncodedData) {
+	  var base64EncodedBytes = System.Convert.FromBase64String(base64EncodedData);
+	  return System.Text.Encoding.UTF8.GetString(base64EncodedBytes);
+	}
+
+	#endregion
+
   }
 }
---
+++ DRUGNORTHQT/Clients/DNQTDataAccessLayer/DALNew/DAL_Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace DNQTDataAccessLayer.DALNew {
+  public class DAL_Account:Connection {
+	public void CheckConnectionDbFromConfig(ref bool blnSuccess) {
+	  blnSuccess=false;
+
+	  string strConnection = "";
+	  GetStringConnectionDecodeFromConfig(ref strConnection);
+	  CheckConnectionDbFromString(ref blnSuccess,strConnection);
+	}
+
+	public void CheckConnectionDbFromString(ref bool blnSuccess,string strConnection) {
+	  blnSuccess=false;
+	  try {
+		using(SqlConnection connection = new SqlConnection(strConnection)) {
+		  try {
+			connection.Open();
+			blnSuccess=true;
+			return;
+		  } catch(SqlException) {
+			blnSuccess=false;
+			return;
+		  }
+		}
+	  } catch(Exception e) {
+		string str = e.Message;
+	  }
+	}
+
+  }
+}
