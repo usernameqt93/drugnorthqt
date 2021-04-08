696ae586375ff98dd5d0e3ca1c9952db74175c89
tuanpq <quoctuan931113@gmail.com>
2021-04-08 12:54:51 +0800

    update WebLogQT.csproj add Assets/admin js scripts

---
+++ WebLogQT/WebLogQT/Assets/admin/js/userController.js
@@ -0,0 +1,31 @@
+﻿var user = {
+  init: function () {
+	user.registerEvents();
+  },
+  registerEvents: function () {
+	$('.btn-active').off('click').on('click', function (e) {
+	  e.preventDefault();
+
+	  var varBtn = $(this);
+	  //var varABCId = varBtn.data('id');
+	  //var varABCDId = parseFloat(varBtn.data('id'));
+	  //var varId = parseInt(varBtn.data('id'));
+	  var varId = varBtn.data('id');
+	  $.ajax({
+		url: "/TenAreaAdmin/User/JsonResultChangeStatus",
+		data: { longId: varId },
+		dataType: "json",
+		type:"POST",
+		success: function (response) {
+		  console.log(response);
+		  if (response.status == true) {
+			varBtn.text('Kích hoạt');
+		  } else {
+			varBtn.text('Khóa');
+		  }
+		}
+	  });
+	});
+  }
+}
+user.init();
---
+++ WebLogQT/WebLogQT/Assets/admin/plugins/ckeditor/config.js
@@ -0,0 +1,23 @@
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	// Define changes to default configuration here. For example:
+	// config.language = 'fr';
+	// config.uiColor = '#AADC6E';
+
+  //config.extraPlugins = 'syntaxhighlight';
+  config.syntaxhighlight_lang = 'csharp';
+  config.syntaxhighlight_hideControls = true;
+  config.language = 'vi';
+  config.filebrowserBrowseUrl = '/Assets/admin/plugins/ckfinder/ckfinder.html';
+  config.filebrowserImageBrowseUrl = '/Assets/admin/plugins/ckfinder.html?Type=Images';
+  config.filebrowserFlashBrowseUrl = '/Assets/admin/plugins/ckfinder.html?Type=Flash';
+  config.filebrowserUploadUrl = '/Assets/admin/plugins/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Files';
+  config.filebrowserImageUploadUrl = '/Data';
+  config.filebrowserFlashUploadUrl = '/Assets/admin/plugins/ckfinder/core/connector/aspx/connector.aspx?command=QuickUpload&type=Flash';
+
+  CKFinder.setupCKEditor(null, '/Assets/admin/plugins/ckfinder/');
+};
