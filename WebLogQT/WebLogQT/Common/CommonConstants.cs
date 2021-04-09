afd3f2ef5c218f043c47ae841d7ba281740bb610
tuanpq <quoctuan931113@gmail.com>
2021-04-09 15:29:56 +0800

    update WebLogQT.csproj add _LeftMenu.cshtml CtlHomeAccountController.cs

---
+++ WebLogQT/WebLogQT/Common/CommonConstants.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebLogQT.Common {
+  internal static class CommonConstants {
+	internal static string USER_SESSION = "USER_SESSION";
+  }
+}
